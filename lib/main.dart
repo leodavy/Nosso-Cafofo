4a2672ea25234e82fd1a22ac2ac8577bf23cd7f2
Davy Leonardo <leo.davy@hotmail.com>
2022-11-07 00:45:00 -0300

    flutterfire add

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:nosso_cafofo/screens/login.dart';
 import 'package:nosso_cafofo/screens/splash.dart';
 
-void main() {
-  runApp(MaterialApp(
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(NossoCafofo());
+}
+
+  class NossoCafofo extends StatelessWidget{
+    
+    @override
+    Widget build(BuildContext context){
+      return MaterialApp(
     debugShowCheckedModeBanner: false,
     routes: {
       "/": (context) => Splash(),
       '/login':(context) => Login(), 
       //'/Register':(context) => Register()
     },
-  ));
+  );
 }
+  }
--- lib/screens/Login.dart
+++ lib/screens/Login.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
-
-
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
