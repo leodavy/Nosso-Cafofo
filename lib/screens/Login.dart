f2ab07bfb9503fc9a515799e0fbf0aeb4f7f186f
Davy Leonardo <leo.davy@hotmail.com>
2022-11-07 22:29:55 -0300

    buildando a tela de login

--- lib/screens/Login.dart
+++ lib/screens/Login.dart
@@ -1,27 +1,87 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class Login extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
-  }
-    Widget build(BuildContext context){
-      return new MaterialApp(
-        home: new Scaffold(
-          body: new Container(
-            color:Colors.blueGrey[600],
-            child: new Container(
-              height: 175.05 ,
-              width: 127,
-              child: 
-                Image.asset('assets/images/Login/Icone.png')
-              ),
+class Login extends StatelessWidget {
+  const Login({Key? key}) : super(key: key);
 
-            )
-        )
-        )
-        ;
+  @override
+
+
+    Widget build(BuildContext context){
+      return  Scaffold(
+        body:  Container(
+          padding: EdgeInsets.only(
+              top: 60, 
+              left: 40,
+              right: 40
+              ), 
+              color:Colors.blueGrey[600],
+              child: ListView(
+                children: <Widget> [
+                  SizedBox(
+                    width: 128,
+                    height: 128,
+                    child: Image.asset("assets/images/Login/Icone.png"),
+                  ),
+                  SizedBox(
+                    width: 20), //para separar iconece das lacunas email/senha  
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText:  "E-mail: ",
+                      labelStyle: TextStyle(color:
+                       Colors.black38,
+                       fontWeight: FontWeight.w400,
+                       fontSize: 20,
+                      )
+                  ),
+                  style: TextStyle(
+                    fontSize: 20
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                    TextFormField(
+                    keyboardType: TextInputType.text,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText:  "senha: ",
+                      labelStyle: TextStyle(color:
+                       Colors.black38,
+                       fontWeight: FontWeight.w400,
+                       fontSize: 20,
+                      )
+                  ),
+                  style: TextStyle(fontSize: 20),
+                  ),
+                    Container(
+                      height: 40,
+                      alignment: Alignment.centerRight,
+                      child: 
+                      TextButton(
+                        style: TextButton.styleFrom(
+                          textStyle: const TextStyle(fontSize: 20,
+                          backgroundColor: Colors.black
+                          ),
+                         
+                        ),
+                        onPressed: () {  },
+                        child: Text(
+                          "Esqueceu a senha?",
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            decorationThickness: 2.85,
+                            
+                          ),
+                        )
+                      )
+                       
+                    )
+                 ]
+                 ),
+                  ),
+              );
     }
 
   }
