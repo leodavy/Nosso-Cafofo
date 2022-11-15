21f4fb4703e79625db96f6181dd7e2a0d71c9876
Davy Leonardo <leo.davy@hotmail.com>
2022-11-15 00:34:34 -0300

    building screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:nosso_cafofo/screens/Register.dart';
-import 'package:nosso_cafofo/screens/login.dart';
-import 'package:nosso_cafofo/screens/splash.dart';
+import 'package:nosso_cafofo/screens/Login.dart';
+import 'package:nosso_cafofo/screens/Splash.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,18 +10,17 @@ void main() async {
   runApp(NossoCafofo());
 }
 
-  class NossoCafofo extends StatelessWidget{
-    
-    @override
-    Widget build(BuildContext context){
-      return MaterialApp(
-    debugShowCheckedModeBanner: false,
-    initialRoute: "/Splash",
-    routes: {
-      "/Splash": (context) => Splash(),
-      '/login':(context) => Login(), 
-      '/ForgotPassword':(context) => ForgotPassword()
-    },
-  );
-}
+class NossoCafofo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: "/Splash",
+      routes: {
+        "/Splash": (context) => Splash(),
+        '/login': (context) => Login(),
+        '/ForgotPassword': (context) => ForgotPassword()
+      },
+    );
   }
+}
