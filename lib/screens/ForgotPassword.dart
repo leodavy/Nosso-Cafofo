0ffd577a729c54be77211bc2343a4c244a8057f6
Rafael Oliveira dos Santos <rafaeloliveira5345@gmail.com>
2022-12-04 13:12:29 -0300

    Add files via upload

---
+++ lib/screens/ForgotPassword.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import '../utils/colors_util.dart';
+
+class ForgotPassword extends StatefulWidget {
+  const ForgotPassword({super.key});
+
+  @override
+  State<ForgotPassword> createState() => _ForgotPasswordState();
+}
+
+class _ForgotPasswordState extends State<ForgotPassword> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        color: hexStringToColor("#A5c9CA"),
+      ),
+    );
+  }
+}
