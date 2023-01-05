13eba2cb5f1a6f07d9c135d2a623563d3c638d47
Davy Leonardo <leo.davy@hotmail.com>
2023-01-04 23:25:39 -0300

    cafofo screen att

--- lib/screens/Cafofo.dart
+++ lib/screens/Cafofo.dart
@@ -9,75 +9,39 @@ class Cafofo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: hexStringToColor("#A5c9CA"),
-        body: Stack(children: <Widget>[
-          Positioned(
-            left: MediaQuery.of(context).size.width / 20,
+        body: Column(children: <Widget>[
+          SizedBox(
+            height: 300,
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height / 6,
             width: MediaQuery.of(context).size.width / 3,
-            top: MediaQuery.of(context).size.height / 3,
-            child: Container(
-              height: MediaQuery.of(context).size.height / 7,
-              width: MediaQuery.of(context).size.width / 3,
-              decoration: BoxDecoration(
-                  color: hexStringToColor("#E7F6F2"),
-                  borderRadius: BorderRadius.circular(50)),
-              child: ElevatedButton.icon(
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: hexStringToColor("#E7F6F2")),
-                label: Text("Compras"),
-                icon: Icon(
-                    size: 100,
-                    Icons.shopping_cart,
-                    color: hexStringToColor("2C3333")),
-                onPressed: () {
-                  Navigator.pushNamed(context, "/Shopping");
-                },
-              ),
+            decoration: BoxDecoration(
+                color: hexStringToColor("#E7F6F2"),
+                borderRadius: BorderRadius.circular(20)),
+            child: IconButton(
+              onPressed: () {
+                Navigator.pushNamed(context, "/Shopping");
+              },
+              icon: Icon(Icons.shopping_cart, size: 100),
             ),
           ),
-          Positioned(
-              left: MediaQuery.of(context).size.width / 20,
-              width: MediaQuery.of(context).size.width / 2,
-              top: MediaQuery.of(context).size.height / 2,
-              child: Container(
-                height: MediaQuery.of(context).size.height / 7,
-                width: MediaQuery.of(context).size.width / 4,
-                decoration: BoxDecoration(
-                    color: hexStringToColor("#E7F6F2"),
-                    borderRadius: BorderRadius.circular(15)),
-                child: IconButton(
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: hexStringToColor("#E7F6F2")),
-                  icon: Icon(
-                      size: 100,
-                      Icons.wallet,
-                      color: hexStringToColor("2C3333")),
-                  onPressed: () {
-                    Navigator.pushNamed(context, "/Finances");
-                  },
-                ),
-              )),
-          Positioned(
-              left: MediaQuery.of(context).size.width / 2,
-              width: MediaQuery.of(context).size.width / 2,
-              top: MediaQuery.of(context).size.height / 3,
-              child: Container(
-                height: MediaQuery.of(context).size.height / 8,
-                width: MediaQuery.of(context).size.height / 3,
-                decoration: BoxDecoration(
-                    color: hexStringToColor("#E7F6F2"),
-                    borderRadius: BorderRadius.circular(15)),
-                child: IconButton(
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: hexStringToColor("#E7F6F2")),
-                  icon: Icon(
-                      size: 100,
-                      Icons.cleaning_services_sharp,
-                      color: hexStringToColor("2C3333")),
-                  onPressed: () {
-                    Navigator.pushNamed(context, "/Tasks");
-                  },
-                ),
-              ))
+          SizedBox(
+            height: 30,
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height / 6,
+            width: MediaQuery.of(context).size.width / 3,
+            decoration: BoxDecoration(
+                color: hexStringToColor("#E7F6F2"),
+                borderRadius: BorderRadius.circular(20)),
+            child: IconButton(
+              onPressed: () {
+                Navigator.pushNamed(context, "/Finances");
+              },
+              icon: Icon(Icons.money, size: 100),
+            ),
+          ),
         ]));
   }
 }
