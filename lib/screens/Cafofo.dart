639dc9d0d836c64eeb84e46c120358ff6763c428
Davy Leonardo <leo.davy@hotmail.com>
2023-01-05 23:02:39 -0300

    Cafofo Screen done

--- lib/screens/Cafofo.dart
+++ lib/screens/Cafofo.dart
@@ -9,39 +9,65 @@ class Cafofo extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: hexStringToColor("#A5c9CA"),
-        body: Column(children: <Widget>[
-          SizedBox(
-            height: 300,
-          ),
-          Container(
-            height: MediaQuery.of(context).size.height / 6,
-            width: MediaQuery.of(context).size.width / 3,
-            decoration: BoxDecoration(
-                color: hexStringToColor("#E7F6F2"),
-                borderRadius: BorderRadius.circular(20)),
-            child: IconButton(
-              onPressed: () {
-                Navigator.pushNamed(context, "/Shopping");
-              },
-              icon: Icon(Icons.shopping_cart, size: 100),
+        body: Center(
+            child: Wrap(
+          crossAxisAlignment: WrapCrossAlignment.center,
+          spacing: 32,
+          runSpacing: 20,
+          children: <Widget>[
+            Container(
+              height: MediaQuery.of(context).size.height / 6,
+              width: MediaQuery.of(context).size.width / 3,
+              decoration: BoxDecoration(
+                  color: hexStringToColor("#E7F6F2"),
+                  borderRadius: BorderRadius.circular(20)),
+              child: IconButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, "/Shopping");
+                },
+                icon: Icon(Icons.shopping_cart, size: 100),
+              ),
             ),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          Container(
-            height: MediaQuery.of(context).size.height / 6,
-            width: MediaQuery.of(context).size.width / 3,
-            decoration: BoxDecoration(
-                color: hexStringToColor("#E7F6F2"),
-                borderRadius: BorderRadius.circular(20)),
-            child: IconButton(
-              onPressed: () {
-                Navigator.pushNamed(context, "/Finances");
-              },
-              icon: Icon(Icons.money, size: 100),
+            Container(
+              height: MediaQuery.of(context).size.height / 6,
+              width: MediaQuery.of(context).size.width / 3,
+              decoration: BoxDecoration(
+                  color: hexStringToColor("#E7F6F2"),
+                  borderRadius: BorderRadius.circular(20)),
+              child: IconButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, "/Tasks");
+                },
+                icon: Icon(Icons.cleaning_services_rounded, size: 100),
+              ),
             ),
-          ),
-        ]));
+            Container(
+              height: MediaQuery.of(context).size.height / 6,
+              width: MediaQuery.of(context).size.width / 3,
+              decoration: BoxDecoration(
+                  color: hexStringToColor("#E7F6F2"),
+                  borderRadius: BorderRadius.circular(20)),
+              child: IconButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, "/Finances");
+                },
+                icon: Icon(Icons.monetization_on, size: 100),
+              ),
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height / 6,
+              width: MediaQuery.of(context).size.width / 3,
+              decoration: BoxDecoration(
+                  color: hexStringToColor("#E7F6F2"),
+                  borderRadius: BorderRadius.circular(20)),
+              child: IconButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, "/Residents");
+                },
+                icon: Icon(Icons.people, size: 100),
+              ),
+            )
+          ],
+        )));
   }
 }
